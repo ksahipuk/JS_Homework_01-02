a9be6a3a02d237d35e8666accf7360f3b5a6abf0
ksahipuk <felis.manul@gmail.com>
2016-08-24 20:13:29 +0300

    bugs fix

--- js-01/js/script.js
+++ js-01/js/script.js
@@ -1,28 +1,30 @@
 function pow(y, n) {
     var powResult = 1;
-    if (n % 1 == 0) {
-        if (n < 0) {
-            for (var i = n; i < 0; i++) {
-                powResult = powResult * y;
+
+    if (y === "number" && n === "number") {
+        if (n % 1 == 0) {
+            if (n < 0) {
+                for (var i = n; i < 0; i++) {
+                    powResult = powResult * y;
+                }
+                return (1 / powResult);
+            } else if (n == 0) {
+                return (1);
+            } else {
+                for (var i = 0; i < n; i++) {
+                    powResult = powResult * y;
+                }
+                return (powResult);
             }
-            return (1 / powResult);
-        } else if (n == 0) {
-            return (1);
         } else {
-            for (var i = 0; i < n; i++) {
-                powResult = powResult * y;
-            }
-            return (powResult);
+            alert( 'Дробные степени не поддерживаются' )
         }
     } else {
-        alert( 'Дробные степени не поддерживаются' )
+    alert( 'Ошибка! Введите целое число' )
     }
 }
 
-var y;
-var n;
-
-y = prompt( 'Введите число' );
-n = prompt( 'Введите степень' );
+var y = prompt( 'Введите число' );
+var n = prompt( 'Введите степень' );
 
 console.log(y + ' в степени ' + n + ' = ' + pow(y, n));
--- js-02/js/script.js
+++ js-02/js/script.js
@@ -2,19 +2,26 @@ var userNames = [];
 var checkLogin = false;
 
 for (var i = 0; i < 5; i++) {
-  userNames[i] = prompt('Введите имя пользователя');
+    userNames[i] = prompt('Введите имя пользователя');
+    if (userNames[i] === "") {
+        alert('Вы ввели пустую строку')
+        break
+    };
 }
 
 var currentLogin = prompt('Введите Ваше имя пользователя');
 
 for (var i = 0; i < userNames.length; i++) {
-  if (currentLogin == userNames[i]) {
-    checkLogin = true;
-  }
+    if (currentLogin === "") {
+        alert('Вы ввели пустую строку')
+        break
+    };
+    if (currentLogin == userNames[i]) {
+        checkLogin = true;
+    }
 }
-
 if (checkLogin) {
-  alert(currentLogin + ' вы успешно вошли');
+    alert(currentLogin + ' вы успешно вошли');
 } else {
-  alert('Такого пользователя не существует');
+    alert('Такого пользователя не существует');
 }
